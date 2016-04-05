10a81d0cbc584b91487a9977bd359b1b075f4c18
Oleg Tsigenov <o.tsigenov@fz-juelich.de>
2016-04-05 14:45:38 +0200

    bkp

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -12,32 +12,34 @@ node 'localhost.localdomain' {
   } ->
   class { 'postgresql::server': }
 
-  postgresql::server::pg_hba_rule { 'Dcache_01':
-    description => 'local is for Unix domain socket connections only',
-    type        => 'local',
-    database    => 'all',
-    user        => 'all',
-    address     => '',
-    auth_method => 'trust',
-  }
+  create_resources(postgresql::server::pg_hba_rule, hiera(pg_hba_rules, 'nodeff'))
 
-  postgresql::server::pg_hba_rule { 'Dcache_02':
-    description => 'local  IPv4',
-    type        => 'host',
-    database    => 'all',
-    user        => 'all',
-    address     => '127.0.0.1/32',
-    auth_method => 'trust',
-  }
+  #  postgresql::server::pg_hba_rule { 'Dcache_01':
+  #    description => 'local is for Unix domain socket connections only',
+  #    type        => 'local',
+  #    database    => 'all',
+  #    user        => 'all',
+  #    address     => '',
+  #    auth_method => 'trust',
+  #  }
 
-  postgresql::server::pg_hba_rule { 'Dcache_03':
-    description => 'local  IPv6',
-    type        => 'host',
-    database    => 'all',
-    user        => 'all',
-    address     => '::1/128',
-    auth_method => 'trust',
-  }
+  #  postgresql::server::pg_hba_rule { 'Dcache_02':
+  #    description => 'local  IPv4',
+  #    type        => 'host',
+  #    database    => 'all',
+  #    user        => 'all',
+  #    address     => '127.0.0.1/32',
+  #    auth_method => 'trust',
+  #  }
+
+  #  postgresql::server::pg_hba_rule { 'Dcache_03':
+  #    description => 'local  IPv6',
+  #    type        => 'host',
+  #    database    => 'all',
+  #    user        => 'all',
+  #    address     => '::1/128',
+  #    auth_method => 'trust',
+  #  }
 
   postgresql::server::role { 'chimera': } ->
   postgresql::server::db { 'chimera':
@@ -59,11 +61,18 @@ node 'localhost.localdomain' {
 
   class { 'dcache':
     package_ensure => hiera('dcache_version'),
-    conf           => hiera('dcache_conf_lofar', 'nodeff'),
+    conf           => hiera('dcache_conf', 'nodeff'),
     admin_ssh_keys => hiera('dc_ssh_pub_keys', 'nodeff'),
-  #      layout_conf => hiera('dcahce_layout'),
-  #      dcache_etc_dir => '/tmp/',
-  #       require => Yumrepo['test-repo-www'],
+    require        => [
+      Package['java-1.8.0-openjdk'],
+      Postgresql::Server::Db['billing'],
+      Postgresql::Server::Db['chimera'],
+      Postgresql::Server::Db['dcache'],
+      Class['postgresql::server']]
+  } ->
+  class { 'dcache::layout':
+    layout_hash => hiera('dcache_layout', 'nodeff'),
+    p_setup     => hiera('pools_setup', 'nodeff'),
   }
 
   package { 'java-1.8.0-openjdk': ensure => 'installed', }
